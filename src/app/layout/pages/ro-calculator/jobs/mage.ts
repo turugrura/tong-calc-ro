74dc7b7a036c4d86e58684647f46d49738e294a8
aphisit khamya <aphisit@primo.mobi>
2023-12-16 21:24:46 +0700

    [bug] added Fire Wall to get magical pene bonus

--- src/app/layout/pages/ro-calculator/jobs/mage.ts
+++ src/app/layout/pages/ro-calculator/jobs/mage.ts
@@ -28,5 +28,24 @@ export class Mage extends CharacterBase {
     },
   ];
   protected readonly _activeSkillList: ActiveSkillModel[] = [];
-  protected readonly _passiveSkillList: PassiveSkillModel[] = [];
+  protected readonly _passiveSkillList: PassiveSkillModel[] = [
+    {
+      inputType: 'dropdown',
+      label: 'Safety Wall',
+      name: 'Safety Wall',
+      dropdown: [
+        { label: '-', isUse: false, value: 0 },
+        { label: 'Lv 1', isUse: true, value: 1 },
+        { label: 'Lv 2', isUse: true, value: 2 },
+        { label: 'Lv 3', isUse: true, value: 3 },
+        { label: 'Lv 4', isUse: true, value: 4 },
+        { label: 'Lv 5', isUse: true, value: 5 },
+        { label: 'Lv 6', isUse: true, value: 6 },
+        { label: 'Lv 7', isUse: true, value: 7 },
+        { label: 'Lv 8', isUse: true, value: 8 },
+        { label: 'Lv 9', isUse: true, value: 9 },
+        { label: 'Lv 10', isUse: true, value: 10 },
+      ],
+    },
+  ];
 }
