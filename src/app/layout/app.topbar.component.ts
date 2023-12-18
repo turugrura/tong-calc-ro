977410d3fb777997d4ec39cf5169e47e50587e57
aphisit khamya <aphisit@primo.mobi>
2023-12-18 22:21:01 +0700

    [release] v1.7.9

--- src/app/layout/app.topbar.component.ts
+++ src/app/layout/app.topbar.component.ts
@@ -105,6 +105,11 @@ export class AppTopBarComponent {
 
   updates: { v: string; date: string; logs: string[] }[] = [
     {
+      v: 'V1.7.9',
+      date: '18-12-2566',
+      logs: ['Added SC offensive skill (Fient Bomb)', 'Added Sin weapons (EP 17.2)', 'Added requested items'],
+    },
+    {
       v: 'V1.7.8',
       date: '16-12-2566',
       logs: [
