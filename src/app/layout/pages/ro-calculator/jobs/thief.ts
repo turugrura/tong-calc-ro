ad8ae28673ee6d296286d3903ac4f1e233017720
aphisit khamya <aphisit@primo.mobi>
2024-01-07 23:02:07 +0700

    [feat] added learnable skill [Hiding]

--- src/app/layout/pages/ro-calculator/jobs/thief.ts
+++ src/app/layout/pages/ro-calculator/jobs/thief.ts
@@ -12,5 +12,26 @@ export class Thief extends CharacterBase {
   protected readonly classNames = ['Thief', 'Thief Cls', 'Thief Class'];
   protected readonly _atkSkillList: AtkSkillModel[] = [];
   protected readonly _activeSkillList: ActiveSkillModel[] = [];
-  protected readonly _passiveSkillList: PassiveSkillModel[] = [ImproveDodgeFn()];
+  Hiding;
+  protected readonly _passiveSkillList: PassiveSkillModel[] = [
+    ImproveDodgeFn(),
+    {
+      name: 'Hiding',
+      label: 'Hiding',
+      inputType: 'dropdown',
+      dropdown: [
+        { label: '-', value: 0, isUse: false },
+        { label: 'Lv 1', value: 1, skillLv: 1, isUse: true },
+        { label: 'Lv 2', value: 2, skillLv: 2, isUse: true },
+        { label: 'Lv 3', value: 3, skillLv: 3, isUse: true },
+        { label: 'Lv 4', value: 4, skillLv: 4, isUse: true },
+        { label: 'Lv 5', value: 5, skillLv: 5, isUse: true },
+        { label: 'Lv 6', value: 6, skillLv: 6, isUse: true },
+        { label: 'Lv 7', value: 7, skillLv: 7, isUse: true },
+        { label: 'Lv 8', value: 8, skillLv: 8, isUse: true },
+        { label: 'Lv 9', value: 9, skillLv: 9, isUse: true },
+        { label: 'Lv 10', value: 10, skillLv: 10, isUse: true },
+      ],
+    },
+  ];
 }
