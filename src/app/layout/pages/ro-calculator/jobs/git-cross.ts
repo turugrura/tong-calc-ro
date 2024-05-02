74e514ed345eee4299e249b1ddd95bc1bf8e0d05
aphisit khamya <aphisit@primo.mobi>
2024-05-02 21:57:44 +0700

    [feat] added Official last updated items

--- src/app/layout/pages/ro-calculator/jobs/git-cross.ts
+++ src/app/layout/pages/ro-calculator/jobs/git-cross.ts
@@ -300,6 +300,45 @@ export class GitCross extends CharacterBase {
         { label: 'Lv 5', isUse: true, value: 5 },
       ],
     },
+    {
+      inputType: 'dropdown',
+      label: 'Cross Impact',
+      name: 'Cross Impact',
+      dropdown: [
+        { label: '-', isUse: false, value: 0 },
+        { label: 'Lv 1', isUse: true, value: 1 },
+        { label: 'Lv 2', isUse: true, value: 2 },
+        { label: 'Lv 3', isUse: true, value: 3 },
+        { label: 'Lv 4', isUse: true, value: 4 },
+        { label: 'Lv 5', isUse: true, value: 5 },
+      ],
+    },
+    {
+      inputType: 'dropdown',
+      label: 'Weapon Blocking',
+      name: 'Weapon Blocking',
+      dropdown: [
+        { label: '-', isUse: false, value: 0 },
+        { label: 'Lv 1', isUse: true, value: 1 },
+        { label: 'Lv 2', isUse: true, value: 2 },
+        { label: 'Lv 3', isUse: true, value: 3 },
+        { label: 'Lv 4', isUse: true, value: 4 },
+        { label: 'Lv 5', isUse: true, value: 5 },
+      ],
+    },
+    {
+      inputType: 'dropdown',
+      label: 'Cloaking Exceed',
+      name: 'Cloaking Exceed',
+      dropdown: [
+        { label: '-', isUse: false, value: 0 },
+        { label: 'Lv 1', isUse: true, value: 1 },
+        { label: 'Lv 2', isUse: true, value: 2 },
+        { label: 'Lv 3', isUse: true, value: 3 },
+        { label: 'Lv 4', isUse: true, value: 4 },
+        { label: 'Lv 5', isUse: true, value: 5 },
+      ],
+    },
   ];
 
   constructor() {
