cee1f34ff1400b191b8df4de1c0789326be1b9ad
aphisit khamya <aphisit@primo.mobi>
2024-05-05 22:51:34 +0700

    [bug] fixed ASPD and Doram job bonus

--- src/app/layout/pages/ro-calculator/jobs/_aspd-table.ts
+++ src/app/layout/pages/ro-calculator/jobs/_aspd-table.ts
@@ -11,20 +11,20 @@ type Y = {
 interface XX extends Y, X {}
 
 export const AspdTable: Partial<Record<ClassName, XX>> = {
-  [ClassName.RuneKnight]: { base: 156, shield: -5, Dagger: -10, Sword: -12, 'Two-Handed Sword': -15, Axe: -8, 'Two-Handed Axe': -12, Mace: -5, Spear: -20, 'Two-Handed Spear': -18 },
-  [ClassName.RoyalGuard]: { base: 156, shield: -5, Dagger: -7, Sword: -5, 'Two-Handed Sword': -13, Axe: -8, 'Two-Handed Axe': -12, Mace: -4, Spear: -10, 'Two-Handed Spear': -10 },
+  [ClassName.RuneKnight]: { base: 156, shield: -5, Dagger: -10, Sword: -12, 'Two-Handed Sword': -15, Axe: -8, 'Two-Handed Axe': -12, Mace: -5, 'Two-Handed Mace': -12, Spear: -20, 'Two-Handed Spear': -18 },
+  [ClassName.RoyalGuard]: { base: 156, shield: -5, Dagger: -7, Sword: -5, 'Two-Handed Sword': -13, Axe: -8, 'Two-Handed Axe': -12, Mace: -4, 'Two-Handed Mace': -10, Spear: -10, 'Two-Handed Spear': -10 },
 
   [ClassName.Ranger]: { base: 156, shield: -8, Dagger: -10, Bow: -9 },
   [ClassName.Minstrel]: { base: 156, shield: -7, Dagger: -12, Bow: -9, Instrument: -4 },
   [ClassName.Wanderer]: { base: 156, shield: -7, Dagger: -12, Bow: -9, Whip: -4 },
 
   [ClassName.Mechanic]: { base: 156, shield: -6, Dagger: -20, Sword: -25, Axe: -5, 'Two-Handed Axe': -8, Mace: -8, 'Two-Handed Mace': -10 },
-  [ClassName.Genetic]: { base: 156, shield: -4, Dagger: -10, Sword: -4, Axe: -8, 'Two-Handed Axe': -11, Mace: -5, 'Two-Handed Mace': -8 },
+  [ClassName.Genetic]: { base: 156, shield: -4, Dagger: -10, Sword: -4, Axe: -8, 'Two-Handed Axe': -11, Mace: -4, 'Two-Handed Mace': -8 },
 
   [ClassName.ArchBishop]: { base: 151, shield: -5, Mace: 0, 'Two-Handed Mace': 0, Rod: -15, 'Two-Handed Rod': -10, Book: 1, Fistweapon: -5 },
   [ClassName.Sura]: { base: 158, shield: -5, Mace: -5, 'Two-Handed Mace': -7, Rod: -10, 'Two-Handed Rod': -12, Fistweapon: -1 },
 
-  [ClassName.GuillotineCross]: { base: 156, shield: -9, Dagger: -2, Sword: -25, Katar: -2, 'left-Dagger': -10, 'left-Sword': -16 },
+  [ClassName.GuillotineCross]: { base: 156, shield: -9, Dagger: -2, Sword: -25, Axe: -40, Katar: -2, 'left-Dagger': -10, 'left-Sword': -16 },
   [ClassName.ShadowChaser]: { base: 156, shield: -4, Dagger: -3, Sword: -7, Bow: -7 },
 
   [ClassName.Sorcerer]: { base: 156, shield: -5, Dagger: -10, Sword: -10, Rod: -5, 'Two-Handed Rod': -15, Book: -5 },
