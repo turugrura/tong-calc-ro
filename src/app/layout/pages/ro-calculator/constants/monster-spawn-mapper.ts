fd705a12f4fef8b46d0455eb52510d1c1b5440c6
aphisit khamya <aphisit@primo.mobi>
2024-05-16 22:47:36 +0700

    [feat] added monster spaw on Farm Lost Valley

--- src/app/layout/pages/ro-calculator/constants/monster-spawn-mapper.ts
+++ src/app/layout/pages/ro-calculator/constants/monster-spawn-mapper.ts
@@ -28,6 +28,7 @@ const Mapper = {
   lhz_dun03: 'Lab 3',
   lhz_dun_n: 'Lab 5',
   ba_pw03: 'Magic Power Plant 2',
+  ba_lost: 'Farm Lost Valley',
 } as const;
 
 export const MonsterGroupNames = [...new Set(Object.values(Mapper))].sort((a, b) => (a > b ? 1 : -1));
