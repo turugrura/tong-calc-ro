15698ce57a17041bde4096641affea34243c5c9f
aphisit khamya <aphisit@primo.mobi>
2024-09-28 00:07:12 +0700

    [feat] added Good Evil Headgears & Sins Fate Weapons

---
+++ src/app/constants/enchant_item/sins_fate_weapon.ts
@@ -0,0 +1,38 @@
+import { Acute, Adamatine, MagicEess, MasterArc, Mettle, Tenacity, aspd15, caster15, ea15, ef15, em15, hitPlus15, sh15 } from "./_basic"
+
+export const sinsFateWeapon4 = [
+  ...ea15,
+  ...ef15,
+  ...em15,
+]
+export const sinsFateWeapon3 = [
+  ...sh15,
+  ...aspd15,
+  ...caster15,
+  ...hitPlus15,
+]
+
+export const goodEvilHead4 = [
+  Acute._1,
+  Acute._2,
+  Acute._3,
+  Tenacity._1,
+  Tenacity._2,
+  Tenacity._3,
+  MasterArc._1,
+  MasterArc._2,
+  MasterArc._3,
+  Adamatine._1,
+  Adamatine._2,
+  Adamatine._3,
+  MagicEess._1,
+  MagicEess._2,
+  MagicEess._3,
+  Mettle._1,
+  Mettle._2,
+  Mettle._3,
+]
+export const goodEvilHead3 = [
+  'Justice_Power',
+  'Injustice_Power',
+]
