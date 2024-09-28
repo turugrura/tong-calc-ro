7ca0780dd3b959050365c0351a5fed2393011d50
aphisit khamya <aphisit@primo.mobi>
2024-09-29 00:43:50 +0700

    [feat] added Dimensions series

--- src/app/jobs/kagerou.ts
+++ src/app/jobs/kagerou.ts
@@ -208,6 +208,19 @@ export class Kagerou extends Ninja {
     RighthandMasteryFn(),
     PureSoulFn(),
     S16thNightFn(),
+    {
+      label: 'Empty Shadow',
+      name: 'Empty Shadow',
+      inputType: 'dropdown',
+      dropdown: [
+        { label: '-', value: 0, isUse: false },
+        { label: 'Lv 1', value: 1, isUse: true },
+        { label: 'Lv 2', value: 2, isUse: true },
+        { label: 'Lv 3', value: 3, isUse: true },
+        { label: 'Lv 4', value: 4, isUse: true },
+        { label: 'Lv 5', value: 5, isUse: true },
+      ],
+    },
   ];
 
   constructor() {
