ed8ade17a660d55002ef3f6ea6d59cfeaed2c6c5
aphisit khamya <aphisit@primo.mobi>
2024-10-20 17:10:01 +0700

    [bug] fixed OSAD enchantments

--- src/app/constants/enchant_item/osad_weapon.ts
+++ src/app/constants/enchant_item/osad_weapon.ts
@@ -1,13 +1,12 @@
-import { aspd15, caster15, ea15, ef15, hitPlus15, sh15, sp15 } from "./_basic"
-
-export const oasdWeapon4 = [
-  ...ea15,
-  ...ef15,
-  ...sp15,
-
-]
+import { aspd15, caster15, ea15, ef15, em15, hitPlus15, sh15 } from "./_basic"
 
 export const oasdWeapon3 = [
+  ...ea15,
+  ...ef15,
+  ...em15,
+]
+
+export const oasdWeapon4 = [
   ...sh15,
   ...aspd15,
   ...hitPlus15,
