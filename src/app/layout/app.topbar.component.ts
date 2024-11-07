b96bfa8e83129ea552279e03a8469d8c90cdad9e
aphisit khamya <apisitk10@gmail.com>
2024-11-07 22:41:15 +0700

    [release] v3.1.6

--- src/app/layout/app.topbar.component.ts
+++ src/app/layout/app.topbar.component.ts
@@ -206,6 +206,13 @@ export class AppTopBarComponent implements OnInit, OnDestroy {
 
   updates: { v: string; date: string; logs: string[] }[] = [
     {
+      v: 'V3.1.6',
+      date: '07-11-2567',
+      logs: [
+        "Added Official updated items",
+      ],
+    },
+    {
       v: 'V3.1.5',
       date: '31-10-2567',
       logs: [
