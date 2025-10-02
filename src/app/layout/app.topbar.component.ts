0c4092201e856c164246ba403f4925e379e02cfd
turugrura <apisitk10@gmail.com>
2025-10-02 23:52:10 +0700

    [feat] added official items

--- src/app/layout/app.topbar.component.ts
+++ src/app/layout/app.topbar.component.ts
@@ -206,6 +206,13 @@ export class AppTopBarComponent implements OnInit, OnDestroy {
 
   updates: { v: string; date: string; logs: string[]; }[] = [
     {
+      v: 'V3.2.15',
+      date: '02-10-2568',
+      logs: [
+        "Added Official updated items",
+      ],
+    },
+    {
       v: 'V3.2.14',
       date: '20-09-2568',
       logs: [
